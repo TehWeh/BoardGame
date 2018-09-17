93b4fd350eb34e57681badd0e4344226310ad99d
Michi <michael.ruderer@uni-ulm.de>
2018-09-17 22:44:33 +0200

    .idea

--- Client/src/util/Renderable.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.state.StateBasedGame;
-
-public interface Renderable {
-    void render(GameContainer gc, StateBasedGame sg, Graphics graphics);
-}
